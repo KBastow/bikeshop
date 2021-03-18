86b60eb42eeb05c81bbf0d7eccea4d0e805254a0
Kelvin Bastow <mtbislegend@gmail.com>
2021-03-18 15:39:49 +0000

    Update 18/03/2021 - 15:39

--- tests/test_app.py
+++ tests/test_app.py
@@ -19,21 +19,21 @@ class TestBase(TestCase):
         return app
 
     def setUp(self):
-        #Will be called before every test
+        # Will be called before every test
         # Create table
         db.create_all()
         # Create test registree
         customer = Customer(first_name = "Kelvin", last_name = "Bastow", email_address = "kelvinbastow@outlook.com")
         product = Products(product = "Canyon Ultimate CFR Disc Di2", quantity = 20, price = 8649)
         orders = Orders(product_id = 1, customer_id = 1, quantity = 1, total_price = 8649, date_ordered = datetime.now())
-        # save users to database
+        # Save users to database
         db.session.add(customer)
         db.session.add(product)
         db.session.add(orders)
         db.session.commit()
 
     def tearDown(self):
-        #Will be called after every test
+        # Will be called after every test
         db.session.remove()
         db.drop_all()
 
@@ -44,9 +44,19 @@ class TestAddCustomer(TestBase):
         response = self.client.get(url_for('add_customer'))
         self.assertEqual(response.status_code, 200)
 
+class TestDeleteCustomer(TestBase):
+    def test_delete_customer(self):
+        response = self.client.get(url_for('delete_customer', cid=1), follow_redirects = True)
+        self.assertEqual(response.status_code, 200)
+
 class TestAddProduct(TestBase):
     def test_add_products(self):
         response = self.client.get(url_for('add_product'))
+        self.assertEqual(response.status_code, 200)
+
+class TestDeleteProduct(TestBase):
+    def test_delete_products(self):
+        response = self.client.get(url_for('delete_product', pid=1), follow_redirects = True)
         self.assertEqual(response.status_code, 200)
 
 class TestAddOrder(TestBase):
@@ -54,7 +64,13 @@ class TestAddOrder(TestBase):
         response = self.client.get(url_for('add_order', cid=1, pid=1))
         self.assertEqual(response.status_code, 200)
 
+class TestDeleteOrder(TestBase):
+    def test_delete_order(self):
+        response = self.client.get(url_for('delete_order', oid=1), follow_redirects =True)
+        self.assertEqual(response.status_code, 200)
+
 #CUSTOMER
+
 class TestAddCustomerDB(TestBase):
     def test_add_customer_db(self):
         response = self.client.post(url_for('add_customer'),
@@ -71,7 +87,7 @@ class TestDeleteCustomerDB(TestBase):
         )
         self.assertIn(b'2',response.data)
 
-#PRODCUT
+#PRODUCT
 
 class TestAddProductDB(TestBase):
     def test_add_product_db(self):
@@ -97,20 +113,20 @@ class TestAddOrderDB(TestBase):
             data = dict(product_id = 1, customer_id = 1, quantity = 1, total_price = 9749, date_ordered = datetime.now()),
             follow_redirects=True
         )
-        self.assertIn(b'1',response.data)
+        self.assertIn(b'5',response.data)
 
-class TestDeleteOrderDB(TestBase):
+class TesToDeleteOrderDB(TestBase):
     def test_delete_order_db(self):
         response = self.client.post(url_for('delete_order', oid=1),
             data = dict(product_id = 1, customer_id = 1, quantity = 1, total_price = 9749, date_ordered = datetime.now()),
             follow_redirects=True
         )
-        self.assertIn(b'2',response.data)
+        self.assertIn(b'5',response.data)
 
-# class TestToUpdateOrder(TestBase):
-#     def test_update_order_db(self):
-#         response = self.client.post(url_for('update_order', oid=1).
-#             data = dict(product_id = 1, customer_id = 1, quantity = 1, total_price = 9749, date_ordered = datetime.now()),
-#             follow_redirects=True
-#         )
-#         self.assertIn(b'3',response.data)
+class TestToUpdateOrderDB(TestBase):
+    def test_update_order_db(self):
+        response = self.client.post(url_for('update_order', oid=1),
+            data = dict(product_id = 1, customer_id = 1, quantity = 1, total_price = 9749, date_ordered = datetime.now()),
+            follow_redirects=True
+        )
+        self.assertIn(b'5',response.data)
